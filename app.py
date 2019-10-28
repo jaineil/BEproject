f104b7904ab68d65a05271d00f41d78996636e77
unknown <akashdeepsingh98@gmail.com>
2019-10-28 22:12:16 +0530

    take some data

--- app.py
+++ app.py
@@ -25,29 +25,30 @@ def process():
     nb = new_notebook()
     json_string = request.form['state']
     datastore = json.loads(json_string)
-    print(datastore)
+    dataset = datastore['dataset']
+    activations = ["Dropout", "MaxPooling"]
     optimizer = str(datastore["optimizer"])
     content = '''
 import tensorflow as tf
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),'''
+dataset = tf.keras.datasets.{}
+(x_train, y_train), (x_test, y_test) = dataset.load_data()
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=({}))'''.format(dataset, datastore["layers"]["1"]["inputsize"])
     
-    for i in range(datastore["layers"]["count"]):
-        inputsize = list(map(int, datastore["layers"][str(i+1)]['inputsize'].rstrip().split()))
-        outputsize = list(map(int, datastore["layers"][str(i+1)]['outputsize'].rstrip().split()))
-        if len(inputsize) == 0:
+    for i in range(2, datastore["layers"]["count"]+1):
+        if datastore["layers"][str(i)]["type"] in activations:
             content = content + '''
-        tf.keras.layers.Dense({}, activation=tf.nn.{}),'''.format(outputsize, datastore["layers"][str(i+1)]["act"])
+        ,tf.keras.layers.Dropout({})'''.format(datastore["layers"][str(i)]["outputsize"])
         else:
             content = content + '''
-        tf.keras.layers.Dense({}, input_shape={}, activation=tf.nn.{}),'''.format(outputsize, 
-        inputsize,
-        datastore["layers"][str(i+1)]["act"])
+        ,tf.keras.layers.Dense({}, activation=tf.nn.{})'''.format(int(datastore["layers"][str(i)]['outputsize']), datastore["layers"][str(i)]["act"])
     
     content = content + '''])
 model.compile(optimizer='{}',
     loss='sparse_categorical_crossentropy',
-    metrics=['accuracy'])'''.format(optimizer)
+    metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test,  y_test, verbose=2)'''.format(optimizer)
     f.write(content)
     nb.cells.append(new_code_cell(content))
     nbformat.write(nb, 'demofile.ipynb')
